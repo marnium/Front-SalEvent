02fe4b8893abb7c11a5f678efb9f5bf85410dc69
Martin Monjaraz Almaraz <marnium97@gmail.com>
2020-10-09 17:58:15 -0500

    Evento de calendario

--- js/servicios.js
+++ js/servicios.js
@@ -11,6 +11,15 @@ var vm = new Vue({
                'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
       daysMonth: [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
       week: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+      type_reserv: 'novalue',
+      count_time: 0,
+      count_people: 0,
+      chairs: 0,
+      tables: 0,
+      tablecloths: 0,
+      assistants: 0,
+      total: 0,
+      day_reserv: null
    },
    methods: {
       nextMonth: function() {
@@ -26,6 +35,64 @@ var vm = new Vue({
             this.month = 11;
             this.year--;
          }
+      },
+      clearFields: function() {
+         console.log('limpiando campos');
+         this.count_time = 0;
+         this.count_people = 0;
+         this.chairs = 0;
+         this.tables = 0;
+         this.tablecloths = 0;
+         this.assistants = 0;
+         this.total = 0;
+      },
+      reservDay: function(w, d) {
+         let date_select = new Date(this.year, this.month, this.calendar[w][d]);
+         console.log('día: ' + this.calendar[w][d]);
+
+         if (this.day_reserv === null) {
+            /**
+             * Es la primera vez que se selecciona una fecha
+             */
+            this.day_reserv = date_select;
+         } else if (!(date_select == this.day_reserv)) {
+            /**
+             * NO es la primera vez que se selecciona una fecha
+             * por lo mismo es necesario cambiar el color de la fecha
+             * seleccionada previamente y después el cambiar el color
+             * de la fecha seleccionada actualmente, el que lanzo el evento
+             */
+            let day_reserv_prev = this.day_reserv;
+            this.day_reserv = date_select;
+            if (day_reserv_prev.getFullYear() == this.year && day_reserv_prev.getMonth() == this.month) {
+               let pos = this.searchDayInCal(day_reserv_prev);
+               let element = document.querySelector('#cal > div:nth-child('+pos.week+')').childNodes[pos.day].firstChild;
+               let className = element.className.split(' ');
+               className.pop();
+               className.pop();
+               className.push('btn-success');
+               element.className = className.join(' ');
+            }
+         }
+         let element = document.querySelector('#cal > div:nth-child('+(w+1)+')').childNodes[d].firstChild;
+         console.log('elemento: ', element.firstChild.nodeValue);
+         let className = element.className.split(' ');
+         className.pop();
+         className.push('btn-warning');
+         className.push('disabled');
+         element.className = className.join(' ');
+      },
+      searchDayInCal: function(date) {
+         let position = {week: 0, day: 0};
+         for (let w = 0; w < this.calendar.length; w++) {
+            for (let d = 0; d < this.calendar[w].length; d++) {
+               if (date.getDay() == this.calendar[w][d]) {
+                  position.week = w + 1;
+                  position.day = d + 1;
+               }
+            }
+         }
+         return position;
       }
    },
    computed: {
