eb8807ce9b219d79c1dc6fec8c732ca9df659de4
Martin Monjaraz Almaraz <marnium97@gmail.com>
2020-10-11 02:32:47 -0500

    Contact Modified

--- contact/index.php
+++ contact/index.php
@@ -4,32 +4,22 @@
    <meta charset="UTF-8" />
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    <title>Contacto</title>
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
    <style>
-      .bg-cont {
+      main > section {
+         background: black url("../img/contact/contact.png") no-repeat scroll;
+         background-size: cover;
+      }
+      .bg-contact {
          background: rgba(172, 169, 169, 0.4);
       }
-      #img {
-         height: auto;
-         border-radius: 25px 25px 0 0;
+      .border-radius-contact {
+         border-radius: 25px 25px 15px 15px;
       }
-      #cont-info {
-         position: absolute;
-         top: 0;
-         left: 0;
-         border-radius: 25px 25px 0 0;
-      }
-      #cont-form {
-         width: 100%;
-         position: absolute;
-         top: 70%;
-         left: 0;
-         border-radius: 0 0 15px 15px;
-      }
-      #bx-form {
+      #container-form {
          border-radius: 0 0 15px 15px;
       }
       #form-contact > div.form-group > input[id] {
@@ -50,7 +40,7 @@
          width: 0;
          height: auto;
          position: absolute;
-         transition: width 0.5s linear;
+         transition: width 0.4s linear;
       }
       .animation-focus .left {
          right: 50%;
@@ -62,26 +52,11 @@
          width: 50%;
          border-bottom: 2px solid #ffc107;
       }
-
-      @media screen and (min-width: 992px) {
-         #img {
+      @media (min-width: 992px) {
+         .border-radius-contact {
             border-radius: 25px;
          }
-         #cont-info {
-            width: 50%;
-            border-radius: 25px 0 0  25px;
-         }
-         #cont-form {
-            top: 0;
-            left: 50%;
-            width: 50%;
-            height: 100%;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            border-radius: 0 25px 25px 0;
-         }
-         #bx-form {
+         #container-form {
             border-radius: 15px;
          }
       }
@@ -91,17 +66,14 @@
 <?php
    include('../partials/home/navigation.html');
 ?>
-   <main class="container h-auto">
-      <section class="w-100" style="position: relative;">
-         <article class="w-100 text-white" style="position: relative; background-color: white;">
-            <img id="img" src="../img/contact/contact.png" alt="Escritorio" class="w-100" />
-            <div id="cont-info" class="h-100 bg-cont px-2 pt-2 px-sm-3 pt-lg-5">
+   <main class="container py-2">
+      <section class="w-100 border-radius-contact">
+         <div class="w-100 d-flex flex-wrap bg-contact border-radius-contact text-white px-lg-3">
+            <article class="mb-5 px-2 pt-4 px-sm-3 col-lg-6 pr-lg-5 pt-lg-5">
                <h3 class="text-center font-weight-bold">Contactanos</h3>
                <p class="text-justify px-lg-3" style="line-height: 1.3;">¿Tienes ganas de contactarnos? Envíenos sus consultas aquí y nos pondremos en contacto con usted lo antes posible</p>
-            </div>
-         </article>
-         <article id="cont-form" class="bg-cont px-lg-3">
-            <div id="bx-form" class="bg-dark text-white px-2 py-3 p-sm-3 pt-lg-5">
+            </article>
+            <article id="container-form" class="bg-dark px-2 py-3 p-sm-3 col-lg-6 pt-lg-5 my-lg-5">
                <form action="" id="form-contact">
                   <div class="form-group input">
                      <label for="full_name">Nombre completo:</label>
@@ -130,8 +102,8 @@
                   </div>
                   <input type="submit" value="Envía un mensaje" class="btn btn-warning btn-block text-white" />
                </form>
-            </div>
-         </article>
+            </article>
+         </div>
       </section>
    </main>
 <?php
