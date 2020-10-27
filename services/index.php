76679d804d01c9448244b75e10fad6b81c15b8fd
Martin Monjaraz Almaraz <marnium97@gmail.com>
2020-10-27 16:57:51 -0600

    Descriptions added to home

--- services/index.php
+++ services/index.php
@@ -91,7 +91,7 @@
             <span aria-hidden="true">&times;</span>
          </button>
       </div>
-      <section id="date-price" class="row no-gutters mb-3">
+      <section id="date-price" class="row no-gutters mb-4">
          <article id="date" class="w-100 text-center pt-2 col-lg-5">
             <h5 class="mb-2 font-weight-bold">Seleccione la fecha</h5>
             <div id="month" class="d-flex justify-content-between align-items-center mb-3 pb-3">
@@ -219,20 +219,47 @@
          </article>
       </section>
       <section id="services" class="mb-5 pl-sm-3 pr-sm-3">
-         <h4 class="font-weight-bold">Contamos con los diferentes servicios</h4>
+         <h4 class="font-weight-bold mb-3">Prestamos servicios para diferentes tipos de eventos</h4>
          <article class="row no-gutters mb-3">
             <div class="mb-3 pl-md-5 pr-md-5 col-lg-6 pl-lg-3 pr-lg-3">
                <img src="../img/services/boda.jpg" alt="Boda" class="img-fluid" style="border-radius: 30px;">
             </div>
             <div class="col-lg-6">
                <h5 class="font-weight-bold">Bodas</h5>
-               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam ex veritatis hic, expedita autem soluta perferendis blanditiis, voluptate non illum maxime? Tempore, quod rerum saepe sequi non quo quae vitae?</p>
+               <p class="text-justify">
+                  Uno de los servicios más importantes en una boda es el de <span class="font-weight-bold">decoración</span>, ya que la misma reflejara el estilo que tendrá la misma.
+                  La decoración más habitual es aquellas con flores y velas que se suele apreciar en las iglesias y en los salones de casamiento del registro civil;
+                  los servicios de decoración para bodas pueden solicitarse en cualquier floristería ya que por lo general, las mismas ofertan paquetes especiales para las bodas.
+               </p>
+               <h6 class="font-weight-bold">Servicios adicionales</h6>
+               <p class="text-justify">
+                  Cada uno de los servicios que mencionamos suele tener una gran relevancia en las bodas, pero sin duda uno de los más importantes es el de <span class="font-weight-bold">fotografía</span>
+                  y <span class="font-weight-bold">video</span> que suelen ofrecer los estudios, y es aquí en donde se les presenta un dilema que les resulta difícil de resolver.
+                  Esto se debe a que se puede contar con amigos o con algún familiar para que se encargue de fotografiar y filmar la boda,
+                  pero se debe tener en cuenta que esta opción no presentará la calidad deseada en un recuerdo tan importante. Aunque pueda resultar algo costosos, los servicios para bodas de fotografía y
+                  video contratados ofrecen paquetes que pueden resultar muy originales en cuanto a la presentación del trabajo finalizado.
+               </p>
             </div>
          </article>
          <article class="row no-gutters">
             <div class="col-lg-6">
                <h5 class="font-weight-bold">Graduaciones</h5>
-               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet sapiente saepe perspiciatis officia, sit quibusdam placeat beatae est eos eligendi nisi adipisci exercitationem deserunt quos. Inventore quasi atque at itaque.</p>
+               <p class="text-justify">
+                  Nuestro salón de evento destaca entre las compañías que organizan fiestas de graduación, porque brinda los mejores servicios en una conveniente relación precio/calidad.
+                  Por esto es el mejor y más solicitado.
+               </p>
+               <p class="font-weight-bold mb-2">
+                  Brindamos:
+               </p>
+               <ul>
+                  <li>Comida exquisita y de buen gusto.</li>
+                  <li>Ambientaciones musicales en vivo, con solistas y ensambles de primer nivel.</li>
+                  <li>Juegos de convivencia que animan al máximo la fiesta, en un ambiente de alegría y seguridad.</li>
+               </ul>
+               <p class="text-justify">
+                  Contamos con paquetes de salones para graduaciones acordes a tus gustos, presupuesto y con facilidades de pago.
+                  La recompensa a todo tu esfuerzo debe ser grande y para lograrlo, contamos con salones y jardines adaptados para que tu celebración sea inolvidable.
+               </p>
             </div>
             <div class="pl-md-5 pr-md-5 col-lg-6 pl-lg-3 pr-lg-3">
                <img src="../img/services/luz.jpg" alt="Iluminación" class="img-fluid" style="border-radius: 30px;">
